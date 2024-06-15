5b218bbb0f1903633225f41a2b0e10f989c3965d
Clovis <clobnjuh@gmail.com>
2024-06-14 23:03:20 -0300

    02-paginas e componentes 04-cadastro de restaurante

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,16 +4,22 @@ import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
+import { SignUp } from './pages/auth/sign-up'
 
 export const router = createBrowserRouter([
-    { 
-        path: '/', 
+    {
+        path: '/',
         element: <AppLayout />,
         children: [{ path: '/', element: <Dashboard /> }],
     },
-    { 
+    {
         path: '/sign-in',
         element: <AuthLayout />,
-        children: [{ path: '/sign-in', element:  <SignIn /> }]
+        children: [{ path: '/sign-in', element: <SignIn /> }]
+    },
+    {
+        path: '/sign-up',
+        element: <AuthLayout />,
+        children: [{ path: '/sign-up', element: <SignUp /> }]
     },
 ])
