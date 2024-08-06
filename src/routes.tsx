7247e8c2329915b288ee996cc97bb78e5573c121
Clovis <clobnjuh@gmail.com>
2024-08-05 21:55:54 -0300

    04-loading and empty states 05-erros e pagina 404

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,17 +7,17 @@ import { Dashboard } from './pages/app/dashboard/dashboard'
 import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
-
+import { Error } from './pages/error'
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <AppLayout />,
-        errorElement: <NotFound />,
+        errorElement: <Error />,
         children: [
             {
                 path: '/',
-                element: <Dashboard />
+                element: <Dashboard />,
             },
             {
                 path: '/orders',
@@ -26,13 +26,21 @@ export const router = createBrowserRouter([
         ],
     },
     {
-        path: '/sign-in',
+        path: '/',
         element: <AuthLayout />,
-        children: [{ path: '/sign-in', element: <SignIn /> }]
+        children: [
+            {
+                path: '/sign-in',
+                element: <SignIn />,
+            },
+            {
+                path: '/sign-up',
+                element: <SignUp />,
+            },
+        ],
     },
     {
-        path: '/sign-up',
-        element: <AuthLayout />,
-        children: [{ path: '/sign-up', element: <SignUp /> }]
+        path: '*',
+        element: <NotFound />,
     },
 ])
