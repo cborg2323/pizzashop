c688e7224c003124be1faf4c963d09b98e792eea
Clovis <clobnjuh@gmail.com>
2024-08-12 23:40:50 -0300

    05-testes 12-criando mocks de acoes

--- src/api/sign-in.ts
+++ src/api/sign-in.ts
@@ -1,9 +1,9 @@
 import { api } from '@/lib/axios'
 
-export interface SiginBody {
+export interface SignInBody {
     email: string
 }
 
-export async function signIn({ email }: SiginBody) {
+export async function signIn({ email }: SignInBody) {
     await api.post('/authenticate', { email })
 }
